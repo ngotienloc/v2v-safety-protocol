dfa54351033b564d36509f70fa42112a692e6298
ngotienloc <ngotienlocf@gmail.com>
2025-11-07 16:03:09 +0700

    Fix Message

--- include/message.h
+++ include/message.h
@@ -3,41 +3,45 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+typedef struct
+{
+    int32_t latitude;
+    int32_t longitude; 
+} gps_t;
 
-#define MSG_MAX_PALYLOAD_LEN 64
+typedef enum { 
+    BROADCAST_BASE_MESSAGE,        // 0
+    BROADCAST_ALERT_MESSAGE,       // 1
+    BROADCAST_EMERGENCY_MESSAGE,   // 2
+    UNICAST_MESSAGE,               // 3
+} message_id;
 
-// Message type
-typedef enum {
-    MSG_TYPE_BROADCAST = 0x01,  // 3
-    MSB_TYPE_UNICAST   = 0x02,  // 2
-    MSB_TYPE_ALERT     = 0x03,  // 1
-    MSB_TYPE_STATUS    = 0x04   // 4
-} msg_type_t; 
 
-// Message structure
 typedef struct {
-    uint8_t type; 
-    uint8_t src_id; 
-    uint8_t dest_id; // Broadcast = 0xFF
-    uint16_t seq;    // Sequence number
-    uint8_t payload_len;
-    uint8_t payload[MSG_MAX_PALYLOAD_LEN]; 
-    uint16_t checksum; 
-    uint8_t priority; 
-
+    uint16_t messageID; 
+    uint32_t vehicleID;
+    uint32_t timestamp;
+    union{
+        struct {
+            uint16_t speed; 
+            uint16_t heading;
+            uint16_t elevation;
+            gps_t gpsnow;
+        } broadcast_base; 
+        struct{
+            uint16_t type_emergency; 
+            gps_t gpsnow; 
+        } broadcast_emergency; 
+        struct {
+            uint16_t lane_id; 
+            uint16_t speed; 
+        } unicast_emergency; 
+    } payload; 
 } message_t;
 
-// API
+void init_broadcast_base(message_t *msg, uint32_t vehicleID, uint16_t speed, uint16_t heading, uint16_t elevation, gps_t gpsnow, uint32_t timestamp);
+void init_broadcast_emergency(message_t *msg, uint32_t vehicleID, uint16_t type_emergency, gps_t gpsnow, uint32_t timestamp);
+void init_unicast_emergency(message_t *msg, uint32_t vehicleID, uint16_t lane_id, uint16_t speed, uint32_t timestamp);
 
-// Create message
-void message_create(message_t *msg, msg_type_t type,
-                    uint8_t src_id, uint8_t dest_id,
-                    const uint8_t *data, uint8_t len, uint16_t seq);
-
-// Checksum    
-bool message_checksum(message_t *msg); 
-
-// Printlog 
-void message_print(const message_t *msg); 
 
 #endif
--- main/utils.c
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-// Checksum
-uint8_t message_checksum(message_t *msg){
-    uint16_t sum = 0; 
-    for(uint8_t i = 0; i < msg->payload_len; i++){
-        sum += msg->payload[i]; 
-    }
-    return (uint8_t)(sum & 0xFF); 
-}
